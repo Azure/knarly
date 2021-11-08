024754afbd965aefb0f5fec89c89b6ff8ec945ef
Jack <jackfrancis@gmail.com>
2021-11-08 11:25:01 -0800

    just 3 nodes for testing

--- test/e2e/azure_test.go
+++ test/e2e/azure_test.go
@@ -173,7 +173,7 @@ var _ = Describe("Workload cluster creation", func() {
 				ClusterName:              clusterName,
 				KubernetesVersion:        e2eConfig.GetVariable(utils.AKSKubernetesVersion),
 				ControlPlaneMachineCount: pointer.Int64Ptr(1),
-				WorkerMachineCount:       pointer.Int64Ptr(100),
+				WorkerMachineCount:       pointer.Int64Ptr(3),
 			},
 			WaitForClusterIntervals:      e2eConfig.GetIntervals(specName, "wait-cluster"),
 			WaitForControlPlaneIntervals: e2eConfig.GetIntervals(specName, "wait-control-plane"),
